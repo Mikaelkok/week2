dd11a0564bec12a9a8f867472c269738a941ed5c
Mikaelkok <49225413+Mikaelkok@users.noreply.github.com>
2020-04-29 22:06:36 +0300

    updated user route
    fixed problems regarding to old commands

--- week2/wop-starters-week2-1/routes/userRoute.js
+++ week2/wop-starters-week2-1/routes/userRoute.js
@@ -3,19 +3,15 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({dest: './uploads/);
+const upload = multer({dest: './uploads/'});
 const userController = require('../controllers/userController');
 
 router.get('/', userController.user_list_get);
 
 router.get('/:id', userController.user_get);
 
-router.post('/', upload.single('user'), (req, res) =>{
-	console.log('tiedosto: ', req.file);
-	userController.user_post(req, res);
-    //res.send('with this endpoint you can add users');
-});
-
+router.post('/', userController.user_post);
+   
 router.put('/', userController.user_put);
 
 router.delete('/:id', userController.user_delete);
